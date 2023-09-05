5a4ac51aa7cb7c35c4103a36b851722db0c040af
ochoaFranco <ochoa.franco132@gmail.com>
2023-09-05 18:43:02 -0300

    se termino el 12

---
+++ src/ejerc_12/Main.java
@@ -0,0 +1,18 @@
+package ejerc_12;
+
+import ejerc_10.Tarea;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        TareaAdmin tarea1 = new TareaAdmin("Tarea1");
+        ArrayList<TareaAdmin> tareas = new ArrayList<TareaAdmin>();
+        tareas = tarea1.crearListaTareas();
+
+        tareas = tarea1.ordenar(tareas);
+        for (TareaAdmin t : tareas) {
+            System.out.println(t.getDescripcion() + " " + t.getPrioridad() + " " + t.getFechaLimite());
+        }
+    }
+}
